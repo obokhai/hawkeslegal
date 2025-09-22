3c5640cb14eab5eff10586c7f37df8c0751da9a0
obokhai <aibormoses1997@gmail.com>
2025-09-22 18:12:43 +0100

    .

--- app/team/page.js
+++ app/team/page.js
@@ -60,15 +60,14 @@ const Team = () => {
   return (
     <>
         <CustomHeader title="Team" />
-        <div className='flex flex-col gap-y-40  min-h-60 bg-white px-16 mt-12 py-20 pt-16'>
-            <div className='flex justify-between text-lg items-center gap-x-12'>
-                <div className='w-5/6 flex justify-between gap-x-12 items-start'>
+        <div className='flex flex-col gap-y-40  min-h-60 bg-white lg:px-16 px-8 mt-12 py-20 pt-16'>
+            <div className='flex max-lg:flex-col justify-between text-lg items-center max-lg:gap-y-6 lg:gap-x-12'>
+                <div className='w-5/6 flex max-lg:flex-col justify-between gap-x-12 gap-y-6 items-start'>
                     <h1 className='text-6xl font-bold Capitalize -mt-3'>Lawyers</h1>
-                    <p className='flex-1 text-gray-400 font-extralight w-[500px] justify-self-end'>At Hawkes Legal, we carefully select lawyers based on their ability to apply intelligence to solve real-world problems. We intentionally recruit and train individuals who possess innovative thinking, knowledge, and the ability to generate solutions while providing passionate advocacy for our clients in all legal matters.</p>
-
+                    <p className='flex-1 text-gray-400 font-extralight lg:w-[500px] justify-self-end'>At Hawkes Legal, we carefully select lawyers based on their ability to apply intelligence to solve real-world problems. We intentionally recruit and train individuals who possess innovative thinking, knowledge, and the ability to generate solutions while providing passionate advocacy for our clients in all legal matters.</p>
                 </div>
-                <span className='w-1/6 flex justify-end -mt-28 '>
-                <Link href="/team" className=' capitalize flex items-center gap-x-3 font-extralight'>view all <MoveRight size={22}/> </Link>
+                <span className='lg:w-1/6 lg:flex lg:justify-end lg:-mt-28 max-lg:ml-32 '>
+                  <Link href="/team" className=' capitalize flex items-center gap-x-3 font-extralight'>view all <MoveRight size={22}/> </Link>
                 </span>
             </div>
         </div>
